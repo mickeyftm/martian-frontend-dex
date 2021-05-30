846153b2c1f165120d8a90c20404134424c018b8
Yeti <martiandefi@gmail.com>
2021-05-31 03:30:31 +0800

    - fixed gitignore file

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,34 @@
+export default {
+  cake: {
+    56: '0xca167f0a8eb9f91b31d40134ce1cc3b8baf4d72f',
+    97: '',
+  },
+  masterChef: {
+    56: '0x842308Ee4Fc31884C4e98D1653c6006Fd1046505',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+  referral: {
+    56: '0xA7bA700f1b134231133F44Fc1056cD2Fc8ac18C5',
+    97: '',
+  },
+}
